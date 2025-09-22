c837f98af0d14eee9fc42f2f546df3c3123f7af1
Xilef Torres <xilefjacobotorres@gmail.com>
2025-09-22 16:48:20 -0700

    Form updated with options of countries and style enhanced

---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,54 @@
+import { useState, type ChangeEvent } from "react";
+import { countries } from "../../Data/countries";
+import styles from './Form.module.css'
+import type { SearchType } from "../../types";
+
+export default function Form() {
+  
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+  
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    return (
+    <form className={styles.form}>
+
+        <div className={styles.field}>
+            <label htmlFor="city">Ciudad:</label>
+            <input
+                id='city' 
+                type="text"
+                name='city'
+                placeholder='Ciudad'
+                value={search.city}
+                onChange={handleChange}/>
+        </div>
+        <div className={styles.field}>
+            <label htmlFor="country">Pais:</label>
+            <select
+                id="country"
+                name="country"
+                value={search.country}
+                onChange={handleChange}
+            >
+                <option value="">-- Seleccione un pais --</option>
+                {countries.map( country => (
+                    <option 
+                        key={country.code}
+                        value={country.code}
+                    >{country.name}</option>
+                ))}
+            </select>
+        </div>
+
+        <input type="submit" value='Consultar Clima' className={styles.submit}/>
+    </form>
+  )
+}
