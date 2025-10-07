6e49ca8aeef0510cec19130348bd2bd776b06045
Xilef Torres <xilefjacobotorres@gmail.com>
2025-10-07 16:18:56 -0700

    implemented zod to work with api response and verification

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,17 @@
 import axios from "axios"
 import type { SearchType } from "../types"
+import z from "zod"
+
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number()
+    })
+})
+
+type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
     const fetchWeather = async (search: SearchType) => {
@@ -8,8 +20,19 @@ export default function useWeather() {
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
-            const data = await axios(geoUrl)
-            console.log(data)
+            const {data} = await axios(geoUrl)
+
+            const lat = data[0].lat
+            const lon = data[0].lon
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+            const {data: weatherResult} = await axios(weatherUrl)
+            const result = Weather.safeParse(weatherResult)
+            if (result.success) {
+                console.log(result.data.name)
+                console.log(result.data.main.temp)
+            }
 
         }catch(error){
             console.log(error)
